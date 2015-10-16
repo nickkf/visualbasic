52b7294d496a3e6745783e367918cba817a8469f
nickkf <nickkalungefarrando@gmail.com>
2015-10-16 17:58:40 +0200

    pregunta2

--- practica3/My Project/Application.Designer.vb
+++ practica3/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplication1.conteo
+            Me.MainForm = Global.WindowsApplication1.pregunta_2
         End Sub
     End Class
 End Namespace
---
+++ practica3/pregunta 2.vb
@@ -0,0 +1,19 @@
+﻿Public Class pregunta_2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n1 As Integer
+        Dim n2 As Integer
+        Dim resultado As String
+
+        n1 = CInt(TextBox1.Text())
+        n2 = CInt(TextBox2.Text())
+
+
+        If n1 > n2 Then
+            resultado = " es divisible "
+        ElseIf n1 < n2 Then
+            resultado = " no es divisible "
+
+        End If
+        MessageBox.Show(resultado)
+    End Sub
+End Class
